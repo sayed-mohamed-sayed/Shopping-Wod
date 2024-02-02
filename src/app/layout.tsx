d594ff3dc68fb268467493f04d46b86345c5b264
mrsayed2022 <am6776756@gmail.com>
2024-02-02 23:34:08 +0200

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from './components/header'
 import Footer from './components/footer/footer'
+import NextTopLoader from 'nextjs-toploader'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body  className={"font-[cursive]"}>
+        <NextTopLoader color='#07484A'/>
         <Header />
         {children}
         <Footer/>
